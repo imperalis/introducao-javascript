b0cf23d2cd5f90077a47d47fae29a579199fb884
dore <promptoattano@gmail.com>
2022-10-03 15:47:21 -0300

    att

--- js/principal.js
+++ js/principal.js
@@ -10,6 +10,24 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso/(altura*altura);
-tdImc.textContent = imc;
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+
+if(altura <= 0 || altura >= 3.00){
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!"
+}
+
+
+if(alturaEhValida == true && pesoEhValido == true){
+    var imc = peso/(altura*altura);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
